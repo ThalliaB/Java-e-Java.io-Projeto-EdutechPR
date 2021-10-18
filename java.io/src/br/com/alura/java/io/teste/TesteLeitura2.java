03cfee11ce74022bfddd7850ce749d181f8d9395
ThalliaB <thalliabrum60@gmail.com>
2021-10-18 10:25:57 -0300

    Add test of scanner class

--- java.io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java.io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -2,6 +2,8 @@ package br.com.alura.java.io.teste;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -11,7 +13,26 @@ public class TesteLeitura2 {
 		Scanner scanner = new Scanner(new File("contas.csv"));
 		while (scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+//			System.out.println(linha);
+			
+			 Scanner linhaScanner = new Scanner(linha);
+			 linhaScanner.useLocale(Locale.US);
+             linhaScanner.useDelimiter(",");
+             
+             String tipoConta = linhaScanner.next();
+             int agencia = linhaScanner.nextInt();
+             int numero = linhaScanner.nextInt();
+             String titular = linhaScanner.next();
+             double saldo = linhaScanner.nextDouble();
+             
+             System.out.format(new Locale("pt", "BR"), "%s - %04d-%04d, %2s: %05.2f %n", tipoConta, agencia, numero, titular, saldo);
+
+//             System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+
+             linhaScanner.close();
+			
+//			String[] valores = linha.split(",");
+//            System.out.println(Arrays.toString(valores));
 		}
 		
 		scanner.close();
